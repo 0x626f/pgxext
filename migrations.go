15ff69a7dee646b1d55610de1374bce28644e354
0x626f <0x626f.master@gmail.com>
2026-01-18 15:20:35 +0100

    feat(migration): patch migration scrips

--- migrations.go
+++ migrations.go
@@ -19,18 +19,21 @@ func (set MigrationSet) Join(arg MigrationSet) MigrationSet {
 	return append(set, arg...)
 }
 
-func UpMigrations(ctx context.Context, migrations MigrationSet) error {
-	PoolClient.WithContext(ctx).Instance()
+func UpMigrations(config *Config, migrations MigrationSet) error {
+	PoolClient.WithConfig(config).Instance()
+	if err := PoolClient.Err(); err != nil {
+		return err
+	}
 
 	var err error
 	var bundle pgx.Tx
 
-	bundle, err = NewTransaction(ctx)
+	bundle, err = NewTransaction(config.Context)
 	if err != nil {
 		panic(err)
 	}
 
-	err = createMigrationsTable(ctx, bundle)
+	err = createMigrationsTable(config.Context, bundle)
 	if err != nil {
 		panic(err)
 	}
@@ -38,13 +41,13 @@ func UpMigrations(ctx context.Context, migrations MigrationSet) error {
 	for _, migration := range migrations {
 		var exists bool
 
-		err = bundle.QueryRow(ctx,
+		err = bundle.QueryRow(config.Context,
 			`SELECT EXISTS(SELECT 1 FROM migrations WHERE name = $1)`,
 			migration.Name,
 		).Scan(&exists)
 
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(err)
 		}
 
@@ -52,24 +55,24 @@ func UpMigrations(ctx context.Context, migrations MigrationSet) error {
 			continue
 		}
 
-		_, err = bundle.Exec(ctx, migration.UpQuery)
+		_, err = bundle.Exec(config.Context, migration.UpQuery)
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(fmt.Sprintf("migration: %v failed: %v", migration.Name, err))
 		}
 
-		_, err = bundle.Exec(ctx,
+		_, err = bundle.Exec(config.Context,
 			`INSERT INTO migrations (name) VALUES ($1)`,
 			migration.Name,
 		)
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(err)
 		}
 		fmt.Printf("Migration %v processed\n", migration.Name)
 	}
 
-	if err = bundle.Commit(ctx); err != nil {
+	if err = bundle.Commit(config.Context); err != nil {
 		panic(err)
 	}
 
@@ -77,13 +80,16 @@ func UpMigrations(ctx context.Context, migrations MigrationSet) error {
 	return nil
 }
 
-func DownMigrations(ctx context.Context, migrations MigrationSet) error {
-	PoolClient.WithContext(ctx).Instance()
+func DownMigrations(config *Config, migrations MigrationSet) error {
+	PoolClient.WithConfig(config).Instance()
+	if err := PoolClient.Err(); err != nil {
+		return err
+	}
 
 	var err error
 	var bundle pgx.Tx
 
-	bundle, err = NewTransaction(ctx)
+	bundle, err = NewTransaction(config.Context)
 	if err != nil {
 		panic(err)
 	}
@@ -92,13 +98,13 @@ func DownMigrations(ctx context.Context, migrations MigrationSet) error {
 		migration := migrations[index]
 		var exists bool
 
-		err = bundle.QueryRow(ctx,
+		err = bundle.QueryRow(config.Context,
 			`SELECT EXISTS(SELECT 1 FROM migrations WHERE name = $1)`,
 			migration.Name,
 		).Scan(&exists)
 
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(err)
 		}
 
@@ -106,29 +112,29 @@ func DownMigrations(ctx context.Context, migrations MigrationSet) error {
 			continue
 		}
 
-		_, err = bundle.Exec(ctx, migration.DownQuery)
+		_, err = bundle.Exec(config.Context, migration.DownQuery)
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(fmt.Sprintf("migration revert: %v failed: %v", migration.Name, err))
 		}
 
-		_, err = bundle.Exec(ctx,
+		_, err = bundle.Exec(config.Context,
 			`DELETE FROM migrations WHERE name = $1`,
 			migration.Name,
 		)
 		if err != nil {
-			_ = bundle.Rollback(ctx)
+			_ = bundle.Rollback(config.Context)
 			panic(err)
 		}
 		fmt.Printf("Migration %v reverted\n", migration.Name)
 	}
 
-	err = dropMigrationsTable(ctx, bundle)
+	err = dropMigrationsTable(config.Context, bundle)
 	if err != nil {
 		panic(err)
 	}
 
-	if err = bundle.Commit(ctx); err != nil {
+	if err = bundle.Commit(config.Context); err != nil {
 		panic(err)
 	}
 
